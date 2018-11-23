08dad6ca3d02acbcef0f9f7bde7c631bbb196ce8
jeffersonwagner <jeffersonwagner@gmail.com>
2018-11-23 09:28:48 -0200

    api integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Observable} from 'rxjs';
+import { map } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@Injectable()
 export class AppComponent {
-  title = 'movies';
+  constructor(private http: HttpClient) { }
+
+  configUrl = 'http://www.omdbapi.com/?s=' + this.paramSearch + '&apikey=5731dd3c';
+  private _paramSearch: string;
+  private _results: object[];
+
+  getData(): Observable<Array<any>> {
+    return this.http.get<Array<any>>(`${this.configUrl}`);
+  }
+
+  filter() {
+    this.getData().subscribe(res => {
+      // @ts-ignore
+      this.results = res.Search;
+    });
+  }
+
+  get paramSearch(): string {
+    return this._paramSearch;
+  }
+
+  set paramSearch(value: string) {
+    this._paramSearch = value;
+  }
+
+  get results(): object[] {
+    return this._results;
+  }
+
+  set results(value: object[]) {
+    this._results = value;
+  }
 }
